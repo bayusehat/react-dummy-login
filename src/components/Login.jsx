c5e7c6c47d09891a7e9e18375b66ae69da0bd7e8
Arya Bayu <aryabayu23@gmail.com>
2023-02-19 19:08:23 +0700

    login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,34 +1,40 @@
 import React, {Component} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faKey } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faKey, faEye} from '@fortawesome/free-solid-svg-icons';
 
 class Login extends Component{
     render(){
         return(
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-4">
-                    <h2>Login Sistem</h2>
-                        <form>
-                            <div className="form-group">
-                                <label className="text-left">
-                                    <FontAwesomeIcon icon={faUser} />
-                                    Username </label>
-                                <input type="email" className="form-control" placeholder="Masukan username anda"  required/>
-                            </div>
-                            <div className="form-group">
-                                <label for="exampleInputPassword1"> <FontAwesomeIcon icon={faKey} /> Password</label>
-                                <input type="password" className="form-control" placeholder="Password" />
-                            </div>
-                            <div class="form-check">
-                                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                                <label className="form-check-label" for="exampleCheck1">Check me out</label>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-4 col-md-offset-4">
+                    <div className="login-panel panel panel-default">
+                        <div className="panel-heading">
+                            <h3 className="panel-title">Please Sign In</h3>
+                        </div>
+                        <div className="panel-body">
+                            <form>
+                                <div className="form-group">
+                                    <label className="text-left">
+                                        <FontAwesomeIcon icon={faUser} />
+                                        Username </label>
+                                    <input type="email" className="form-control" placeholder="Masukan username anda"  required/>
                                 </div>
-                                <button type="submit" class="btn btn-primary btn-lg btn-block">Submit</button>   
-                        </form>
+                                <div className="form-group">
+                                    <label for="exampleInputPassword1"> <FontAwesomeIcon icon={faKey} /> Password</label>
+                                    <input type="password" className="form-control" placeholder="Password" />
+                                </div>
+                                <button type="submit" className="btn btn-success btn-sm btn-block">LOGIN ADMIN</button>
+                                <button type="submit" className="btn btn-info btn-sm btn-block">Register Mahasiswa Baru</button>   
+                            </form>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <a href=""> <FontAwesomeIcon icon={faEye} /> Lupa Password?</a>
                     </div>
                 </div>
-            </div> 
+            </div>
+        </div>
         );
     }
 }
